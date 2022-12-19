6f39028a7debf1274907aa1186706af0ef12058f
Shahriar <msrahman@sfu.ca>
2022-12-18 21:00:42 -0800

    return time state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TimePicker from './components/TimePicker';
-import logo from './logo.svg';
 
 function App() {
+  const [time, setTime] = useState('00:00');
   return (
     <div className="App" style={{marginLeft: '50%', marginTop:'10%'}}>
       <header className="App-header">
         <TimePicker minuteStep={1}
+          disabledTimes={[{startTime:'13:00', endTime:'22:20'},
+          {startTime:'07:30', endTime:'10:20'}]}
+          setTime={setTime}
+        />
+        <TimePicker minuteStep={1}
           disabledTimes={[{startTime:'19:30', endTime:'22:20'},
           {startTime:'07:30', endTime:'10:20'}]}
+          setTime={setTime}
         />
       </header>
+      {time}
     </div>
   );
 }
--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -6,39 +6,96 @@ interface IProps {
     name?: string;
     label?: string;
     minuteStep?: number;
-    disabledTimes?: IDisabletime[]
+    disabledTimes?: IDisabletime[];
+    setTime: (time: string) => void;
 }
 
-const TimePicker = ({ name = 'time', label = 'Time' , minuteStep=1, disabledTimes=[]}: IProps) => {
-    
+const TimePicker = ({ name = 'time', label = 'Time', minuteStep = 1, disabledTimes = [], setTime }: IProps) => {
     const hours = generateHours();
     const minutes = generateMunites(minuteStep);
     const disabledMunitesSet = genetateDisabledMinutesSet(disabledTimes!);
     const disabledHoursSet = generateDisabledHoursSet(disabledMunitesSet);
     const [hour, setHour] = useState<number>(new Date().getHours());
     const [minute, setMinute] = useState<number>(new Date().getMinutes());
+    const [timepickerVisibile, setTimepickerVisibile] = useState(false);
+    const [unavalaibleError, setUnavalaibleError] = useState(false);
+
+    const handleSelectHour = (hour: number) => {
+        setUnavalaibleError(false);
+        if (disabledHoursSet.has(hour)) {
+            setUnavalaibleError(true);
+        }
+        else {
+            setHour(hour)
+        }
+    }
+    const handleSelectMinute = (minute: number) => {
+        setUnavalaibleError(false);
+        if (disabledMunitesSet.has((hour * 60) + minute)) {
+            setUnavalaibleError(true);
+        }
+        else {
+            try {
+                setMinute(minute)
+
+            } catch(err) {
+
+            } finally {
+                let time = ((hour > 9) ? hour : "0" + hour) + " : " + ((minute > 9) ? minute : "0" + minute);
+                setTime(time);
+            }
+
+            // setTimepickerVisibile(!timepickerVisibile);
+            // handleSetTime();
+        }
+    }
+
+    const handleSetTime = () => {
+        setUnavalaibleError(false);
+        if (disabledHoursSet.has(hour) || disabledMunitesSet.has((hour * 60) + minute)) {
+            setUnavalaibleError(true);
+        }
+        else {
+            let time = ((hour > 9) ? hour : "0" + hour) + " : " + ((minute > 9) ? minute : "0" + minute);
+            setTime(time);
+            setTimepickerVisibile(false);
+        }
+    }
     return (
-        <div className='time'>
-            <div className='time-section'>
-                {hours.map((item) => (
-                    // <span className={hour===item.value ? 'hourActive' : 'hour'} 
-                    <span className={disabledHoursSet.has(item.value) ? 'time-disabled'
-                        : hour===item.value ? 'hourActive' : 'hour'} 
-                        key={item.value}
-                        onClick={() => setHour(item.value)}
-                    >{item.hour}</span>
-                ))}
-            </div>
-            <div className='time-section'>
-                {minutes.map((item) => (
-                    // <span className={minute===item.value ? 'hourActive' : 'hour'}
-                    <span className={disabledMunitesSet.has((hour*60)+item.value) ? 'time-disabled' 
-                            : minute===item.value ? 'hourActive' : 'hour' 
+        <div>
+            <small style={{ marginTop: '-20px', display: unavalaibleError ? 'block' : 'none' }}>Slected time is not available</small>
+            <button className='timepicker-btn' onClick={() => setTimepickerVisibile(!timepickerVisibile)}>
+                {(disabledHoursSet.has(hour) || disabledMunitesSet.has(minute))
+                    ? label
+                    : ((hour > 9) ? hour : "0" + hour) + " : " + ((minute > 9) ? minute : "0" + minute)
+                }
+                {/* (hour > 9 ? hour : "0"+hour : minute > 9 ? minute : "0"+minute) */}
+            </button>
+            <div className='time' style={{ display: timepickerVisibile ? 'flex' : 'none' }}>
+                <div className='time-section'>
+                    {hours.map((item) => (
+                        // <span className={hour===item.value ? 'hourActive' : 'hour'} 
+                        <span className={disabledHoursSet.has(item.value) ? 'time-disabled'
+                            : hour === item.value ? 'hourActive' : 'hour'}
+                            key={item.value}
+                            onClick={() => handleSelectHour(item.value)}
+                        >{item.hour}</span>
+                    ))}
+                </div>
+                <div className='time-section'>
+                    {minutes.map((item) => (
+                        // <span className={minute===item.value ? 'hourActive' : 'hour'}
+                        <span className={disabledMunitesSet.has((hour * 60) + item.value) ? 'time-disabled'
+                            : minute === item.value ? 'hourActive' : 'hour'
                         }
-                        key={item.value}
-                        onClick={() => setMinute(item.value)}
-                    >{item.munite}</span>
-                ))}
+                            key={item.value}
+                            onClick={() => handleSelectMinute(item.value)}
+                        >{item.munite}</span>
+                    ))}
+                </div>
+                <button className='timepicker-btn' onClick={handleSetTime}>
+                    Ok
+                </button>
             </div>
         </div>
     )
